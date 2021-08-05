138f6f442ccf059633fc055e20273cf19c8d0cd7
Palak Gupta <palakgupta2712@gmail.com>
2021-08-05 22:22:35 +0530

    Redirect

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import styles from "./RegisterForm.module.css";
 function RegisterForm() {
   const [token, setToken] = useState(localStorage.getItem("token"));
